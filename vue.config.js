eecf2e877f48be3bb6ef49a45faf79aa1f214ad6
vtrofin <victor_trofin@yahoo.com>
2020-11-17 19:06:34 +0900

    simple pre-render setup

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,15 @@
-module.exports = {};
+const path = require('path');
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new PrerenderSPAPlugin({
+        // Required - The path to the webpack-outputted app to prerender.
+        staticDir: path.join(__dirname, 'dist'),
+        // Required - Routes to render.
+        routes: ['/'], // '/about', '/some/deep/nested/route'
+      }),
+    ],
+  },
+};

ce2d74e75783519073593e4f934b6c5a59dd1856
victor_trofin <victor_trofin@yahoo.com>
2020-12-09 15:34:01 +0900

    Revert "setup github logo wip"
    This reverts commit a02e19f2bc6229adb7385ccaf256635a8531dc4d.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,23 +5,32 @@
       <li><router-link to="#">Work</router-link></li>
       <li><router-link to="/contact">Contact</router-link></li>
       <li><a href="https://vtrofin.github.io/">CV</a></li>
-      <li class="auto-margin"><GithubLogo /></li>
+      <li class="auto-margin">
+        <a
+          class="social-link"
+          href="https://github.com/vtrofin"
+          aria-label="View my GitHub profile"
+        >
+          <img :src="githubLogo" :style="{ width: '25px', height: '25px' }" />
+        </a>
+      </li>
     </ul>
   </nav>
 </template>
 
 <script>
+import githubLogo from '../assets/github.svg';
 import HeaderLogo from './HeaderLogo';
-import GithubLogo from './GithubLogo';
 
 export default {
   name: 'Header',
   components: {
-    HeaderLogo,
-    GithubLogo
+    HeaderLogo
   },
   setup(props) {
-    return {};
+    return {
+      githubLogo
+    };
   }
 };
 </script>
@@ -52,7 +61,6 @@ export default {
 .nav-links li {
   max-width: 100%;
   min-width: 0;
-  margin-left: 10px;
 }
 
 .nav-links li.auto-margin {
@@ -60,7 +68,7 @@ export default {
 }
 
 .nav-links li a {
-  padding: 1.5em 0.75em 1.5em 0;
+  padding: 1.5em 0.75em;
   display: block;
   position: relative;
   cursor: pointer;

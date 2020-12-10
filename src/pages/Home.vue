d9fcea62667ceb68f2dcdeddf165aeb294f22096
Victor Trofin <victor_trofin@yahoo.com>
2020-12-10 15:28:57 +0900

    temporarily disable homepage sections

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,21 +2,21 @@
   <h1>This is the homepage</h1>
   <!-- instead of using all these Sections such as Projects, Work, etc, use a General Section component since they are all the same -->
   <GeneralSection />
-  <Projects></Projects>
+  <!-- <Projects></Projects>
   <Work></Work>
   <Skills></Skills>
-  <Interests></Interests>
+  <Interests></Interests> -->
 </template>
 
 <script>
-import Work from '../components/work';
-import Projects from '../components/projects';
-import Skills from '../components/skills';
-import Interests from '../components/interests';
+// import Work from '../components/work';
+// import Projects from '../components/projects';
+// import Skills from '../components/skills';
+// import Interests from '../components/interests';
 import GeneralSection from '../components/general_section';
 
 export default {
   name: 'HomePage',
-  components: { Work, Projects, Skills, Interests, GeneralSection }
+  components: { /*Work, Projects, Skills, Interests,*/ GeneralSection }
 };
 </script>

5bae9de4a118f16eec8c1a6353d807aa0df1fdd0
Victor Trofin <victor_trofin@yahoo.com>
2020-12-10 15:34:29 +0900

    add hero section

---
+++ src/components/hero_component/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    this is the hero component
+  </div>
+</template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <h1>This is the homepage</h1>
   <!-- instead of using all these Sections such as Projects, Work, etc, use a General Section component since they are all the same -->
+  <HeroSection />
   <GeneralSection />
   <!-- <Projects></Projects>
   <Work></Work>
@@ -13,10 +14,11 @@
 // import Projects from '../components/projects';
 // import Skills from '../components/skills';
 // import Interests from '../components/interests';
+import HeroSection from '../components/hero_component';
 import GeneralSection from '../components/general_section';
 
 export default {
   name: 'HomePage',
-  components: { /*Work, Projects, Skills, Interests,*/ GeneralSection }
+  components: { /*Work, Projects, Skills, Interests,*/ GeneralSection, HeroSection }
 };
 </script>

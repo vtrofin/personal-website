7d107b33331fdf7040f8066d31622e145b439ad1
Victor Trofin <victor_trofin@yahoo.com>
2020-12-11 15:29:23 +0900

    basic custom Url

--- src/components/SectionAppLink.vue
+++ src/components/SectionAppLink.vue
@@ -1,14 +1,18 @@
 <template>
-  <a v-if="isExternalLink" href="#"><slot /></a>
-  <router-link v-else to="#"><slot /></router-link>
+  <a v-if="isExternalLink" :href="path" target="_blank" :ariaLabel="ariaLabel" :class="classNames">
+    <slot /> {{ title }}</a
+  >
+  <router-link v-else :to="path" :class="classNames"> <slot /> {{ title }} </router-link>
 </template>
 
 <script>
-import { computed, toRefs } from 'vue';
+import { computed, toRefs, toRef } from 'vue';
 import { RouterLink, useLink } from 'vue-router';
 import { appLinkValidator } from './helpers/validators';
 import { checkExternalPath } from '../helpers';
-console.log(RouterLink.props);
+
+console.log(RouterLink);
+
 export default {
   name: 'SectionAppLink',
   props: {
@@ -17,12 +21,22 @@ export default {
       default: function() {
         return { path: '#', ariaLabel: 'Dummy url link', title: 'Click here' };
       }
-    }
+    },
+    inactiveClass: { type: String, required: false, default: '' }
   },
   setup(props) {
-    const { path } = toRefs(props.options);
+    const { path, ariaLabel, title } = toRefs(props.options);
     const isExternalLink = computed(() => checkExternalPath({ path: path.value }));
-    return { isExternalLink };
+    return { isExternalLink, ariaLabel, path, title, classNames: ['section-link'] };
   }
 };
 </script>
+
+<style scoped>
+.section-link {
+  display: flex;
+  flex-grow: 1;
+  flex-basis: 100%;
+  padding: 20px;
+}
+</style>

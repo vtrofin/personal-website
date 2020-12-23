c33d71ecd8fee6bd9d192608d6a2c7edb15d69ef
vtrofin <victor_trofin@yahoo.com>
2020-12-23 15:01:40 +0900

    clean-up unnecessary store mutations and getters

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,3 @@
-// this is where we'll put the store
-// split the store in multiple modules -> one for the hero component || one for the projects || one for the work experience, etc. etc
-// namespaced modules!
 import { createStore } from 'vuex';
 import heroModule from './modules/hero_module';
 
@@ -14,19 +11,3 @@ const store = createStore({
 });
 
 export default store;
-
-// const store = createStore({
-//   state() {
-//     return {
-//       count: 0
-//     };
-//   },
-//   mutations: {
-//     increment(state) {
-//       state.count++;
-//     }
-//   }
-// });
-// store.commit('increment');
-// store.commit('increment');
-// console.log(store.state.count);
--- src/store/modules/hero_module.js
+++ src/store/modules/hero_module.js
@@ -1,61 +1,26 @@
 export default {
   namespaced: true,
   state: () => ({
-    writeMode: false,
     bashHistory: [],
-    currentLine: '',
     staticText: 'Victors-MBP:~ victor$',
   }),
   getters: {
     getBashHistory: state => {
       return state.bashHistory;
     },
-    isWriteMode: state => {
-      return state.writeMode;
-    },
-    getCurrentLine: state => {
-      return state.currentLine;
-    },
     getStaticText: state => {
       return state.staticText;
     },
   },
   mutations: {
-    append(state, payload) {
-      const { char, charCode, data } = payload;
-      if (data) {
-        state.currentLine += data;
-      } else if (char) {
-        state.currentLine += char;
-      } else {
-        state.currentLine += String.fromCharCode(charCode);
-      }
-    },
-    pushToHistory(state) {
-      const current = state.currentLine;
-      state.bashHistory = [...state.bashHistory, current];
-      state.currentLine = '';
-    },
-    removeCharacter(state) {
-      const line = state.currentLine;
-      if (line.length) {
-        state.currentLine = line.substring(0, line.length - 1);
-      }
+    pushToHistory(state, payload) {
+      const { text } = payload;
+      state.bashHistory = [...state.bashHistory, text];
     },
   },
   actions: {
-    appendChar(context, payload) {
-      const { commit } = context;
-      commit('append', payload);
-    },
-    pushLine({ commit }) {
-      commit('pushToHistory');
-    },
-    removeChar({ commit }) {
-      commit('removeCharacter');
-    },
-    pasteText({ commit }, payload) {
-      commit('append', payload);
+    pushLine({ commit }, payload) {
+      commit('pushToHistory', payload);
     },
   },
 };

fcbe9cb56b9d18f9a2282a455197fe9c9412a1a2
Victor Trofin <victor_trofin@yahoo.com>
2021-01-08 18:00:14 +0900

    wip make summary title

--- src/components/project_item/project_summary.vue
+++ src/components/project_item/project_summary.vue
@@ -1,26 +1,29 @@
 <template>
-  <div class="project-summary">
-    <div class="project-summary-left">
-      <span class="summary-section-subtitle">Position</span>
-      <ul class="summary-section-content">
-        <li>Full Stack Web Engineer</li>
-      </ul>
-      <span class="summary-section-subtitle">Organization</span>
-      <ul class="summary-section-content">
-        <li>Bento&Co</li>
-      </ul>
-      <span class="summary-section-subtitle">Year(s)</span>
-      <ul class="summary-section-content">
-        <li>2018-{{ currentYear }}</li>
-      </ul>
-    </div>
-    <div class="project-summary-right">
-      <span class="summary-section-subtitle">Work</span>
-      <ul class="summary-section-content">
-        <li>Did this</li>
-        <li>Did that</li>
-        <li>Did that other thing</li>
-      </ul>
+  <div class="summary-container">
+    <h3>Summary</h3>
+    <div class="summary">
+      <div class="summary-left">
+        <span class="summary-subtitle">Position</span>
+        <ul class="summary-content">
+          <li>Full Stack Web Engineer</li>
+        </ul>
+        <span class="summary-subtitle">Organization</span>
+        <ul class="summary-content">
+          <li>Bento&Co</li>
+        </ul>
+        <span class="summary-subtitle">Year(s)</span>
+        <ul class="summary-content">
+          <li>2018-{{ currentYear }}</li>
+        </ul>
+      </div>
+      <div class="summary-right">
+        <span class="summary-subtitle">Work</span>
+        <ul class="summary-content">
+          <li>Did this</li>
+          <li>Did that</li>
+          <li>Did that other thing</li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -34,7 +37,7 @@ export default {
 };
 </script>
 <style>
-.project-summary {
+.summary {
   display: flex;
   box-sizing: border-box;
   margin: 3.5rem auto;
@@ -44,33 +47,33 @@ export default {
   border-radius: var(--base-border);
 }
 @media all and (min-width: 600px) {
-  .project-summary {
+  .summary {
     padding: 2rem 1rem;
   }
 }
 
-.project-summary-left,
-.project-summary-right {
+.summary-left,
+.summary-right {
   display: flex;
   flex-direction: column;
   flex-grow: 1;
   padding: 0 0.5rem;
 }
 
-.summary-section-subtitle {
+.summary-subtitle {
   font-size: 0.7rem;
   letter-spacing: 1.5px;
   text-transform: uppercase;
 }
 
-.summary-section-content {
+.summary-content {
   padding: 0;
   list-style-type: none;
   margin: 1rem 0 2.5rem;
   line-height: 1.4;
 }
 
-.summary-section-content:last-child {
+.summary-content:last-child {
   margin-bottom: 0;
 }
 </style>

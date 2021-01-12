4c4dbf89801fdff3b37bb2cb0d26479d7b758e95
Victor Trofin <victor_trofin@yahoo.com>
2021-01-12 10:11:15 +0900

    working send email feature

--- api/[contact].js
+++ api/[contact].js
@@ -1,4 +1,7 @@
 const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
+const { OAuth2 } = google.auth;
+const get = require('lodash.get');
 
 module.exports = (req, res) => {
   // const body = req.body;
@@ -6,32 +9,42 @@ module.exports = (req, res) => {
   const headers = req.headers;
   const query = req.query;
 
-  // https://github.com/nodemailer/nodemailer/issues/1176#issuecomment-717837363
-  const formattedPrivateKey = process.env.GOOGLE_SA_PRIVATE_KEY.replace(/\\n/g, '\n');
+  const clientId = process.env.CLIENT_ID;
+  const secret = process.env.CLIENT_SECRET;
+  const refreshToken = process.env.REFRESH_TOKEN;
+  const sender = process.env.SENDER_EMAIL;
+  const receiver = process.env.RECEIVER_EMAIL;
+  const redirectUrl = process.env.REDIRECT_URL;
 
+  const oAuth2Client = new OAuth2(clientId, secret, redirectUrl);
+  oAuth2Client.setCredentials({ refresh_token: refreshToken });
   const transport = {
     service: 'Gmail',
-    auth: {
-      type: 'OAuth2',
-      user: process.env.MY_EMAIL,
-      serviceClient: process.env.GOOGLE_SA_CLIENT_ID,
-      privateKey: formattedPrivateKey,
-    },
+    auth: { type: 'OAuth2', user: sender, clientId, clientSecret: secret, refreshToken }
   };
   const mailer = nodemailer.createTransport(transport);
+
   const message = {
-    from: process.env.MY_EMAIL,
-    to: [process.env.MY_EMAIL],
-    subject: req.body.subject || '[No subject]',
-    html: req.body.message || '[No message]',
+    from: sender,
+    to: [receiver],
+    subject: get(req, 'body.subject', '[No subject]'),
+    html: get(req, 'body.message', '[No message]')
   };
 
   return mailer
     .sendMail(message)
     .then(result => {
-      res.json({ headers, result });
+      const accepted = result && result.accepted;
+      if (!accepted || !accepted.length) {
+        throw new Error('Failed to send message. No receiver accepted the message');
+      }
+
+      return res
+        .status(200)
+        .json({ headers, messageId: result.messageId, message: 'email sent successfully' }); // headers is dev only
     })
-    .catch((err, info) => {
-      res.json({ headers, err, info });
+    .catch((err, info = {}) => {
+      const message = err.message;
+      return res.status(400).json({ headers, message, response: info.response }); // headers is dev only
     });
 };

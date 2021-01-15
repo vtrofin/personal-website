49e05e85492edb7913d2bfac98b9d1bb5098921a
Victor Trofin <victor_trofin@yahoo.com>
2021-01-15 16:34:25 +0900

    ghost css done

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,14 +1,36 @@
 <template>
   <div class="container">
     <div class="box-container">
-      <div class="box-ghost">ttt ttt ttt</div>
+      <div class="box-ghost">
+        <div class="symbol"></div>
+        <div class="symbol"></div>
+        <div class="symbol"></div>
+        <div class="symbol"></div>
+        <div class="symbol"></div>
+        <div class="symbol"></div>
+
+        <div class="ghost-container">
+          <div class="ghost-eyes-container">
+            <div class="eye" />
+            <div class="eye" />
+          </div>
+          <div class="ghost-feet-container">
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+          </div>
+        </div>
+        <div class="ghost-shadow" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'NotFound',
+  name: 'NotFound'
 };
 </script>
 
@@ -17,6 +39,7 @@ export default {
 .container {
   --base-animation-border: 5px;
   --animation-gray: #574b33;
+  --animation-shadow: #695e46;
 
   box-sizing: border-box;
   position: relative;
@@ -39,9 +62,208 @@ export default {
   position: absolute;
   left: 50%;
   top: 30%;
-  transform: translate3d(-50%, -50%, 0);
+  transform: translate3d(-50%, -30%, 0);
   padding: 15px 25px 25px;
-  border: 1px solid red;
+}
+
+.ghost-container {
+  position: relative;
+  margin: 0 auto;
+  background-color: var(--white);
+  width: 100px;
+  height: 100px;
+  border-top-left-radius: 100px;
+  border-top-right-radius: 100px;
+  animation: updown 3s ease-in-out infinite;
+}
+
+.ghost-eyes-container {
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  height: 12px;
+  width: 70px;
+  transform: translate3d(-50%, 0, 0);
+}
+
+.ghost-eyes-container .eye {
+  width: 12px;
+  height: 12px;
+  background-color: var(--black);
+  border-radius: 50%;
+  margin: 0 10px;
+  position: absolute;
+}
+.ghost-eyes-container .eye:first-child {
+  left: 0;
+}
+.ghost-eyes-container .eye:last-child {
+  right: 0;
+}
+
+.ghost-feet-container {
+  position: absolute;
+  top: 100%;
+  left: 0;
+  right: 0;
+  display: flex;
+}
+
+.ghost-feet-container > div {
+  flex-grow: 1;
+  position: relative;
+  top: -10px;
+  height: 20px;
+  background-color: var(--white);
+  border-radius: 100%;
+}
+.ghost-feet-container > div:nth-child(2n) {
+  margin: 0;
+  border-top: 10px solid var(--animation-gray);
+  background-color: transparent;
+}
+.ghost-shadow {
+  height: 20px;
+  margin: 0 auto;
+  border-radius: 50%;
+  box-shadow: 0 30px 15px 5px var(--animation-shadow);
+  animation: smallbig 3s ease-in-out infinite;
+}
+
+.symbol {
+  opacity: 0.2;
+  animation-name: shine;
+  animation-duration: 4s;
+  animation-timing-function: ease-in-out;
+  animation-delay: 3s;
+  animation-iteration-count: infinite;
+}
+.symbol:before,
+.symbol:after {
+  content: '';
+  position: absolute;
+  width: 12px;
+  height: 4px;
+  background: var(--white);
+  border-radius: 5px;
+}
+
+.symbol:nth-of-type(2) {
+  position: absolute;
+  left: -5px;
+  top: 30px;
+  height: 12px;
+  width: 12px;
+  border: 4px solid;
+  border-radius: 50%;
+  border-color: var(--white);
+  animation-delay: 1.3s;
+}
+.symbol:nth-of-type(3) {
+  animation-duration: 3s;
+  animation-delay: 0.5s;
+}
+.symbol:nth-of-type(4) {
+  animation-duration: 6s;
+  animation-delay: 1.6s;
+}
+.symbol:nth-of-type(5) {
+  position: absolute;
+  right: 5px;
+  top: 40px;
+  height: 8px;
+  width: 8px;
+  border: 3px solid;
+  border-radius: 50%;
+  border-color: var(--white);
+  animation-duration: 1.7s;
+  animation-delay: 7s;
+}
+.symbol:nth-of-type(6) {
+  animation-duration: 2s;
+  animation-delay: 6s;
+}
+
+.symbol:first-of-type:before,
+.symbol:first-of-type::after {
+  bottom: 65px;
+  left: 0;
+}
+.symbol:first-of-type:before {
+  transform: rotate(45deg);
+}
+.symbol:first-of-type::after {
+  transform: rotate(-45deg);
+}
+
+.symbol:nth-of-type(2):before,
+.symbol:nth-of-type(2)::after,
+.symbol:nth-of-type(5):before,
+.symbol:nth-of-type(5)::after {
+  content: none;
+}
+
+.symbol:nth-of-type(3):before,
+.symbol:nth-of-type(3)::after {
+  top: 5px;
+  left: 40px;
+}
+.symbol:nth-of-type(3):before {
+  transform: rotate(90deg);
+}
+.symbol:nth-of-type(3):after {
+  transform: rotate(180deg);
+}
+
+.symbol:nth-of-type(4):before,
+.symbol:nth-of-type(4)::after {
+  width: 15px;
+  top: 10px;
+  right: 30px;
+}
+.symbol:nth-of-type(4):before {
+  transform: rotate(45deg);
+}
+.symbol:nth-of-type(4)::after {
+  transform: rotate(-45deg);
+}
+
+.symbol:nth-of-type(6):before,
+.symbol:nth-of-type(6)::after {
+  width: 15px;
+  bottom: 65px;
+  right: -5px;
+}
+.symbol:nth-of-type(6):before {
+  transform: rotate(90deg);
+}
+.symbol:nth-of-type(6)::after {
+  transform: rotate(180deg);
+}
+
+@keyframes updown {
+  0% {
+    transform: translateY(5px);
+  }
+  50% {
+    transform: translateY(15px);
+  }
+  100% {
+    transform: translateY(5px);
+  }
+}
+
+@keyframes smallbig {
+  0% {
+    width: 90px;
+  }
+  50% {
+    width: 100px;
+  }
+
+  100% {
+    width: 90px;
+  }
 }
 
 @keyframes shine {

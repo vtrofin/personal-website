a1d460e86f98ef6103c7f73396fb260a89e0e575
vtrofin <victor_trofin@yahoo.com>
2021-01-20 14:01:39 +0900

    close button

--- src/layouts/ToolBoxWrapper.vue
+++ src/layouts/ToolBoxWrapper.vue
@@ -3,8 +3,13 @@
     :class="'toolbox-container' + (toolboxActive ? ' ' + 'toolbox-open' : '')"
     @click.self="handleBlur"
   >
-    <div class="toolbox-menu" />
-    <!-- <nav class="toolbox-menu"> <div class="profile" /> <div class="list" /> </nav> <button id="close-button">Close</button> -->
+    <nav class="toolbox-menu">
+      <section class="profile-container">
+        <div class="profile" />
+        <div class="list" />
+      </section>
+      <button id="close-button" @click.prevent="handleBlur">Close</button>
+    </nav>
   </div>
 
   <div :class="'container' + (toolboxActive ? ' ' + 'toolbox-open' : '')">
@@ -43,6 +48,7 @@ export default {
 </script>
 <style>
 #app > .container {
+  --base-translate-unit: 50px;
   min-height: 100vh;
   display: grid;
   grid-template-rows: auto 1fr auto;
@@ -53,8 +59,17 @@ export default {
 }
 
 #app > .toolbox-open:nth-child(2) {
-  transform: translate3d(50px, 50px, 0);
+  transform: translate3d(var(--base-translate-unit), var(--base-translate-unit), 0);
   transition: transform 0.3s;
+}
+@media all and (min-width: 600px) {
+  #app > .toolbox-open:nth-child(2) {
+    transform: translate3d(
+      calc(1.5 * var(--base-translate-unit)),
+      calc(1.5 * var(--base-translate-unit)),
+      0
+    );
+  }
 }
 
 .toolbox-container {
@@ -87,5 +102,37 @@ export default {
 .toolbox-open .toolbox-menu {
   transform: translate3d(0, 0, 0);
   transition: transform 0.2s linear;
+}
+
+#close-button {
+  position: absolute;
+  right: 1rem;
+  top: 1rem;
+  width: 1rem;
+  height: 1rem;
+  overflow: hidden;
+  text-indent: 1rem;
+  border: none;
+  cursor: pointer;
+  background-color: transparent;
+  color: transparent;
+  outline: none;
+}
+#close-button:before,
+#close-button:after {
+  content: '';
+  position: absolute;
+  width: 2px;
+  height: 100%;
+  top: 0;
+  left: 50%;
+  background: #b8b7ad;
+}
+
+#close-button:before {
+  transform: rotate(45deg);
+}
+#close-button:after {
+  transform: rotate(-45deg);
 }
 </style>

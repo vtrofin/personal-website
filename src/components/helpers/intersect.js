0943742f8070c505fac3cff797fa16d7b2c541de
Victor Trofin <victor_trofin@yahoo.com>
2021-01-22 17:41:33 +0900

    remove console logs

--- src/components/helpers/intersect.js
+++ src/components/helpers/intersect.js
@@ -61,8 +61,6 @@ export const getCliObserver = ({
           const isPortraitBug =
             orientation === 'portrait' && action === 'leave' && direction === 'up';
           const isLandscapeBug = orientation === 'landscape' && direction === 'no-scroll';
-          console.log('orentation ---> ', orientation);
-          console.log('scrollState -->', scrollState);
           isPortraitBug || isLandscapeBug ? undefined : cliWrapperActiveText.value.blur();
         } else {
           cliWrapperActiveText.value.blur();

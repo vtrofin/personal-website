0057df60b7bbe0a6f89740fb87e5c0841c36e782
vtrofin <victor_trofin@yahoo.com>
2021-02-08 22:02:42 +0900

    merge changes

--- src/store/modules/companies_module.js
+++ src/store/modules/companies_module.js
@@ -5,23 +5,25 @@ export default {
       {
         project: 'bertrandco',
         type: 'work',
+        item_title: '',
         excerpt:
           'Working as a full-stack web engineer to build features that vastly improve shipping and logistics ',
         path: 'https://bertrandco.com/',
-        ariaLabel: 'View Bertrand Co website'
+        ariaLabel: 'View Bertrand Co website',
       },
       {
         project: 'smoothjapan',
         type: 'work',
-        excerpt: 'I did a little bit of work here ',
+        item_title: '',
+        excerpt: 'Started my professional career by doing markup for Wordpress websites',
         path: 'https://twitter.com/smoothjapan',
-        ariaLabel: 'View Smooth Japan twitter profile.'
-      }
-    ]
+        ariaLabel: 'View Smooth Japan twitter profile.',
+      },
+    ],
   }),
   getters: {
     getAllCompanies: state => {
       return state.companies;
-    }
-  }
+    },
+  },
 };

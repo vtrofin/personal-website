edb66b6603839168632e28416095a1965a4abd9e
vtrofin <victor_trofin@yahoo.com>
2021-02-11 12:25:37 +0900

    push staff text

--- src/store/modules/staff_module.js
+++ src/store/modules/staff_module.js
@@ -2,13 +2,11 @@ export default {
   namespaced: true,
   state: () => ({
     project: {
+      type: 'project',
       excerpt:
         'Internal dashboard to monitor operations in Ship&co. Complete with KPIs, Analytics and Data visualization',
-      description: ['a longer text describing what you did for that project'],
-      // logoClass: `shipandco-logo`,
-      // backgroundClass: `shipandco-background`,
-      type: 'project',
-      item_title: 'A long title for the project description', // will not overwrite the homepage short title
+      item_title: 'Staff Dashboard',
+      role: 'Full-stack web engineer',
     },
   }),
   getters: {

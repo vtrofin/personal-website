abf6fa40794d033e47d6e2e50afccc9ffbe8edcd
vtrofin <victor_trofin@yahoo.com>
2021-02-12 23:25:31 +0900

    replace toolbox with user

--- libs/fa/font_awesome.js
+++ libs/fa/font_awesome.js
@@ -1,10 +1,29 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas, faTools, faEnvelope, faHome } from '@fortawesome/free-solid-svg-icons';
+import {
+  fas,
+  faTools,
+  faEnvelope,
+  faHome,
+  faInfoCircle,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
 import { faPython, faJs, faNodeJs, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-const importedIcons = [fas, far, faEnvelope, faPython, faJs, faNodeJs, faGithub, faTools, faHome];
+const importedIcons = [
+  fas,
+  far,
+  faEnvelope,
+  faPython,
+  faJs,
+  faNodeJs,
+  faGithub,
+  faTools,
+  faHome,
+  faInfoCircle,
+  faUser,
+];
 library.add(...importedIcons);
 
 export { FontAwesomeIcon };

65452b537ed00b29e01366168b04404dea9a0969
vtrofin <victor_trofin@yahoo.com>
2021-04-05 23:57:11 +0900

    edit stock&co text

--- src/components/project_item/stockandco.vue
+++ src/components/project_item/stockandco.vue
@@ -2,74 +2,27 @@
   <div class="project-content">
     <h2>The Project</h2>
     <p>
-      Stock&Co is a Shopify app that enables syncing of inventory levels (product quantities) across
-      multiple stores. It addresses a major inconvenience because there is no full support for i18n
-      in Shopify and users need to create separate stores for each language they require. At the
-      moment the app is a prototype, it is used primarily internally but there is intent to release
-      it in beta after a complete re-write with Vue3 and Fastify.
+      Stock&Co is a private Shopify app that enables syncing of inventory levels across multiple
+      stores.
     </p>
   </div>
   <ProjectImage
     alt-text="stock&co list of products with quantities"
     url="/stockandco_variants.png"
-    caption="Stock&co enables syncing of product quantities across multiple Shopify stores"
-  />
-  <div class="project-content">
-    <h2>The problem</h2>
-    <p>
-      In early 2018 a decision was made to externalize order fulfillment for
-      <a
-        href="https://www.bentoandco.com/"
-        :class="$props.modifier ? 'content-link' + ' ' + $props.modifier : 'content-link'"
-        target="_blank"
-        rel="noopener"
-        aria-label="Click to see this awesome url"
-      >
-        <span>Bento&Co</span>
-      </a>
-      stores to a 3rd party warehouse and, consequently, this required Stock&co to integrate with
-      the 3rd party systems. Also, with a growing team in Bento&Co, there was a need to unify
-      product creation in all the Bento&Co stores.
-    </p>
-  </div>
-  <ProjectImage
-    alt-text="stock&co list of synced shopify stores"
-    url="/stockandco_shops.png"
-    caption="It is easy to connect a Shopify store in Stock&co"
+    caption=""
   />
   <div class="project-content">
     <h2>Contribution</h2>
-    <p>
-      I worked back-end side to create the product and inventory levels sync between the 3rd party
-      systems, Stock&co and Shopify. The main challenges for this task were a tight deadline and an
-      insufficient integration between the 3rd party system and Shopify. Basically, Stock&co had to
-      act as an in-between Shopify and the 3rd party system. Tasks included:
-    </p>
     <ul class="content-list">
-      <li>
-        Restructure the Stock&Co MongoDB database to save the relevant order data required by the
-        3rd party
-      </li>
-      <li>
-        Restructure and enhance merging of product data by SKU inside Stock&co
-      </li>
+      <li>Restructure the Stock&Co MongoDB database to save the relevant product data</li>
+      <li>Restructure and enhance merging of product data by SKU inside Stock&co</li>
       <li>
         Create a REST API with endpoints to fetch product data and inventory levels from Stock&co
-        and push inventory updates to Stock&co and subsequently to Shopify
+        and push inventory updates to Shopify
       </li>
-      <li>
-        Set up user validation and authorization through transactional emails
-      </li>
-      <li>
-        Basic support for Shopify multi-locations
-      </li>
+      <li>Set up user validation and authorization through transactional emails</li>
+      <li>Basic support for Shopify multi-locations</li>
       <li>UI enhancements - e.g. a multi-select tag-cloud</li>
-      <li>
-        Post Covid19 pandemic the partnership with the 3rd party ended and I was in charge of
-        refocusing Stock&co to its core- inventory level sync. I also completely rewrote the unified
-        product creation feature, relying on Shopify web-hooks for syncing product data across
-        multiple Shopify stores instead of having a custom product creation flow.
-      </li>
     </ul>
   </div>
 

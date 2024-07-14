7476c93659bdf1a67ae04ebd08c3d263ffb93317
Victor Trofin <victor_trofin@yahoo.com>
2024-07-14 20:56:36 +0900

    remove plugin vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tsconfigPaths from "vite-tsconfig-paths";
-import vercel from 'vite-plugin-vercel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vercel(),
     tsconfigPaths({
       loose: true,
     }),
